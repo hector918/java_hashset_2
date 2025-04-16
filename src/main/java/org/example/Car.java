eb51107af86ff1a3c5cd07911ee1bc5e9765e01d
hector918 <hector_zhong@outlook.com>
2025-04-16 17:15:19 -0400

    finished

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -3,5 +3,26 @@ package org.example; // Make sure this matches your package
 import java.util.Objects; // For equals() and hashCode()
 
 public class Car {
+    private final String model, brand;
+    public Car(String model, String brand){
+        if(model.isEmpty()){
+            throw new IllegalArgumentException("model can not be empty.");
+        }else{
+            this.model = model;
+        }
 
+        if(brand.isEmpty()){
+            throw new IllegalArgumentException("brand can not be empty.");
+        }else{
+            this.brand = brand;
+        }
+    }
+
+    public String getModel(){
+        return this.model;
+    }
+
+    public String getBrand(){
+        return this.brand;
+    }
 }
--- src/main/java/org/example/CarService.java
+++ src/main/java/org/example/CarService.java
@@ -1,9 +1,59 @@
 package org.example; // Make sure this matches your package
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class CarService {
 
+    private final Set<Car> carList;
 
+    public CarService(){
+        this.carList = new HashSet<>();
+    }
+
+    public void addCar(Car car){
+        if(car == null){
+            throw new IllegalArgumentException("car can not be null.");
+        }
+
+        carList.add(car);
+    }
+    public boolean removeCar(Car car){
+        try{
+            if(car == null){
+                throw new IllegalArgumentException("car can not be null.");
+            }
+
+            if(carList.contains(car)){
+                carList.remove(car);
+                return true;
+            }else{
+                return false;
+            }
+        }catch (Exception e){
+            System.out.println(e.toString());
+            return false;
+        }
+    }
+
+    public int getCarCount(){
+        return carList.size();
+    }
+
+    public Set<Car> getCarsByMake(String brand){
+        if(brand == null){
+            throw new IllegalArgumentException("Brand can not be null.");
+        }
+
+        if(brand.isEmpty()){
+            throw new IllegalArgumentException("Brand can not be empty.");
+        }
+
+        Set<Car> ret = new HashSet<>();
+        for(Car car : this.carList){
+            if(car.getBrand().equalsIgnoreCase(brand)){
+                ret.add(car);
+            }
+        }
+        return ret;
+    }
 }
